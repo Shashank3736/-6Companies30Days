7d7ef6e415de398e49a1af072e6696a3ab64ecc4
Shreyash Raj <shreyashr267@gmail.com>
2023-01-03 11:19:32 +0530

    solved 7

---
+++ Python/Microsoft/7_course_schedule.py
@@ -0,0 +1,25 @@
+# Leetcode 207: Course Schedule
+# https://leetcode.com/problems/course-schedule/
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        def helper(i, visited, path, graph):
+            if visited[i] == 1:
+                return True
+            if visited[i] == 2:
+                return False
+            visited[i] = 2
+            for j in graph[i]:
+                if not helper(j, visited, path, graph):
+                    return False
+            visited[i] = 1
+            path.append(i)
+            return True
+        graph = [[] for _ in range(numCourses)]
+        for i, j in prerequisites:
+            graph[i].append(j)
+        visited = [0] * numCourses
+        path = []
+        for i in range(numCourses):
+            if not helper(i, visited, path, graph):
+                return False
+        return True
