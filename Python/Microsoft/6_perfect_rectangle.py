23f686dc6252b3400dee801b91104b9e0592c92d
Shreyash Raj <shreyashr267@gmail.com>
2023-01-03 11:17:38 +0530

    solved 6 in python

---
+++ Python/Microsoft/6_perfect_rectangle.py
@@ -0,0 +1,14 @@
+# Leetcode 391: Perfect Rectangle
+# https://leetcode.com/problems/perfect-rectangle/
+class Solution:
+    def isRectangleCover(self, rectangles: List[List[int]]) -> bool:
+        if not rectangles:
+            return False
+        area = 0
+        corners = set()
+        for x1, y1, x2, y2 in rectangles:
+            area += (x2 - x1) * (y2 - y1)
+            corners ^= {(x1, y1), (x1, y2), (x2, y1), (x2, y2)}
+        if len(corners) != 4 or area != (max(x for x, y in corners) - min(x for x, y in corners)) * (max(y for x, y in corners) - min(y for x, y in corners)):
+            return False
+        return True
