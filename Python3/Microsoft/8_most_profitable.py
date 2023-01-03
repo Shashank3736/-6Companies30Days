028e64060871dbef4cf30c32c0af6310720c81a0
Shreyash Raj <shreyashr267@gmail.com>
2023-01-03 12:19:47 +0530

    Added gitignore file to ignore incomplete folder

--- Python3/Microsoft/8_most_profitable.py
+++
@@ -1,9 +0,0 @@
-# Leetcode 2467: Most Profitable Path in a Tree
-# https://leetcode.com/problems/most-profitable-path-in-a-tree/
-class Solution:
-    def mostProfitablePath(self, edges: List[List[int]], bob: int, amount: List[int]) -> int:
-        """
-        Leetcode 2467: Most Profitable Path in a Tree
-        Pretty hard question. I don't know how to solve it.
-        """
-        pass
