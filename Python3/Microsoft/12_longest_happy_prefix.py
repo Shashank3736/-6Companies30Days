4f363798eee45bf679a8aec11af1758ee2544d53
Shreyash Raj <shreyashr267@gmail.com>
2023-01-03 13:56:06 +0530

    solved 12

---
+++ Python3/Microsoft/12_longest_happy_prefix.py
@@ -0,0 +1,22 @@
+# Leetcode 1392: Longest Happy Prefix
+# https://leetcode.com/problems/longest-happy-prefix/
+class Solution:
+    def longestPrefix(self, s: str) -> str:
+        n = len(s)
+        if n == 1:
+            return ''
+        if n == 2:
+            return s[0]
+        dp = [0] * n
+        for i in range(1, n):
+            j = dp[i - 1]
+            while j > 0 and s[i] != s[j]:
+                j = dp[j - 1]
+            if s[i] == s[j]:
+                j += 1
+            dp[i] = j
+        return s[:dp[n - 1]]
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.longestPrefix('ababab'))
